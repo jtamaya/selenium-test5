ce18ba1c0b3266ff0baeac86603d5ee4dba58cae
Amaya <jonathan.amaya@softwareone.com>
2023-03-07 16:17:22 -0500

    First commit

---
+++ src/main/java/selenium_test2/new_selenium_test/App.java
@@ -0,0 +1,13 @@
+package selenium_test2.new_selenium_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/selenium_test2/new_selenium_test/AppTest.java
@@ -0,0 +1,20 @@
+package selenium_test2.new_selenium_test;
+
+import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AppTest 
+{
+	@Test
+	public void buildTest() {
+		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://www.browserstack.com/");
+		String verifyBrowserStackTitle = driver.getTitle();
+		assertEquals("Most Reliable App & Cross Browser Testing Platform | BrowserStack2",verifyBrowserStackTitle);
+	}
+}
