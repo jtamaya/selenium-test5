e5718941f138d59cace798ce25bc231b1b954630
jtamaya <117089420+jtamaya@users.noreply.github.com>
2023-03-08 09:52:08 -0500

    Update AppTest.java

--- src/test/java/selenium_test2/new_selenium_test/AppTest.java
+++ src/test/java/selenium_test2/new_selenium_test/AppTest.java
@@ -11,7 +11,7 @@ public class AppTest
 {
 	@Test
 	public void buildTest() {
-		System.setProperty("webdriver.chrome.driver", "./src/drivers/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://www.browserstack.com/");
 		String verifyBrowserStackTitle = driver.getTitle();
