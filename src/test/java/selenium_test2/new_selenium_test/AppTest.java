50002575861fd41a857054246d9c9c96659e9683
Amaya <jonathan.amaya@softwareone.com>
2023-03-08 13:28:56 -0500

    added options

--- src/test/java/selenium_test2/new_selenium_test/AppTest.java
+++ src/test/java/selenium_test2/new_selenium_test/AppTest.java
@@ -6,13 +6,19 @@ import static org.testng.Assert.assertEquals;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class AppTest 
 {
 	@Test
 	public void buildTest() {
 		System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver");
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--headless");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--window-size=1920x1080");
+		WebDriver driver = new ChromeDriver(options);
 		driver.navigate().to("https://www.browserstack.com/");
 		String verifyBrowserStackTitle = driver.getTitle();
 	}
