5ac78ffad63b498c8c91033cc618fa5a3167660f
Amaya <jonathan.amaya@softwareone.com>
2023-03-09 11:58:30 -0500

    divided classess

---
+++ src/test/java/selenium_test2/new_selenium_test/ChromeTest.java
@@ -0,0 +1,5 @@
+package selenium_test2.new_selenium_test;
+
+public class ChromeTest {
+
+}
---
+++ src/test/java/selenium_test2/new_selenium_test/FirefoxTest.java
@@ -0,0 +1,45 @@
+package selenium_test2.new_selenium_test;
+
+import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+
+public class FirefoxTest 
+{
+	WebDriver driver;
+	FirefoxOptions firefoxOptions;
+	
+	
+	@Test
+	public void buildTestFireFox() throws MalformedURLException {
+		System.setProperty("webdriver.firefox.driver", "src/drivers/FirefoxDriver/geckodriver-v0.32.2-linux64.tar.gz");
+		firefoxOptions = new FirefoxOptions();
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), firefoxOptions);
+		testCase(driver);
+	}
+	
+	@AfterTest
+	public void terminate() {
+		driver.close();
+	}
+	
+	public void testCase(WebDriver driver) {
+		driver.navigate().to("https://www.browserstack.com/");
+		String verifyBrowserStackTitle = driver.getTitle();
+		assertEquals("Most Reliable App & Cross Browser Testing Platform | BrowserStack",verifyBrowserStackTitle);
+		System.out.println(verifyBrowserStackTitle);
+	}
+	
+	
+}
