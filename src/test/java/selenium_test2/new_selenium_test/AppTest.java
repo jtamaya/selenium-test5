d5fb750c48d8457eef7a7f7cb9be2ff2f2b943ca
Amaya <jonathan.amaya@softwareone.com>
2023-03-09 11:06:32 -0500

    refactor

--- src/test/java/selenium_test2/new_selenium_test/AppTest.java
+++ src/test/java/selenium_test2/new_selenium_test/AppTest.java
@@ -13,45 +13,56 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
 
 public class AppTest 
 {
+	WebDriver driver;
+	ChromeOptions chromeOptions;
+	EdgeOptions edgeOptions;
+	FirefoxOptions firefoxOptions;
+	
+	@BeforeTest
+	public void buildOptions() {
+		chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--no-sandbox");
+		chromeOptions.addArguments("--headless");
+		chromeOptions.addArguments("--disable-dev-shm-usage");
+		chromeOptions.addArguments("--window-size=1920x1080");
+		
+		edgeOptions = new EdgeOptions();
+		
+		firefoxOptions = new FirefoxOptions();
+	}
+	
 	@Test
-	public void buildTest() throws MalformedURLException {
+	public void buildTestChrome() throws MalformedURLException {
 		System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--no-sandbox");
-		options.addArguments("--headless");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--window-size=1920x1080");
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
-		driver.navigate().to("https://www.browserstack.com/");
-		String verifyBrowserStackTitle = driver.getTitle();
-		assertEquals("Most Reliable App & Cross Browser Testing Platform | BrowserStack",verifyBrowserStackTitle);
-		System.out.println(verifyBrowserStackTitle);
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+		testCase(driver);
+		
 	}
 	
 	@Test
 	public void buildTestEdge() throws MalformedURLException {
 		System.setProperty("webdriver.edge.driver", "src/drivers/msedgedriver");
-		EdgeOptions options = new EdgeOptions();
-		
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), edgeOptions);
+		testCase(driver);
+	}
+	
+	@Test
+	public void buildTestFireFox() throws MalformedURLException {
+		System.setProperty("webdriver.firefox.driver", "src/drivers/FirefoxDriver/geckodriver-v0.32.2-linux64.tar.gz");
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), firefoxOptions);
+		testCase(driver);
+	}
+	
+	public void testCase(WebDriver driver) {
 		driver.navigate().to("https://www.browserstack.com/");
 		String verifyBrowserStackTitle = driver.getTitle();
 		assertEquals("Most Reliable App & Cross Browser Testing Platform | BrowserStack",verifyBrowserStackTitle);
 		System.out.println(verifyBrowserStackTitle);
 	}
 	
-	@Test
-	public void buildTestFireFox() throws MalformedURLException {
-		System.setProperty("webdriver.firefox.driver", "src/drivers/FirefoxDriver/geckodriver-v0.32.2-linux64.tar.gz");
-		FirefoxOptions options = new FirefoxOptions();
-		
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
-		driver.navigate().to("https://www.browserstack.com/");
-		String verifyBrowserStackTitle = driver.getTitle();
-		assertEquals("Most Reliable App & Cross Browser Testing Platform | BrowserStack",verifyBrowserStackTitle);
-		System.out.println(verifyBrowserStackTitle);
-	}
+	
 }
